4d6a8fe146bb6b38a939c371e597456668d96bc5
Mykola <mykola.lev@gmail.com>
2021-04-16 16:00:26 +0300

    fix regex split

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "env_list" {
   value = [
-    for line in compact(split("\n", replace(file(var.env_file_path), "/#.*\n/", ""))) :
+    for line in compact(split("\n", replace(file(var.env_file_path), "/#.*/", ""))) :
     zipmap(
       [
         "name",
